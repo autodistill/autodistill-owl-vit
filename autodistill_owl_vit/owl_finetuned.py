3e579dea1543abb42dbc12f95536c225beca9b8a
Anthony Davis <Anthony.Davis@jhuapl.edu>
2023-08-23 18:28:19 +0000

    add owl fine tuning stuff

--- autodistill_owl_vit/owl_finetuned.py
+++ autodistill_owl_vit/owl_finetuned.py
@@ -1,14 +1,15 @@
 import os
-from dataclasses import dataclass
 
+# from dataclasses import dataclass
 import numpy as np
 import supervision as sv
 import torch
-from autodistill.detection import CaptionOntology, DetectionBaseModel, DetectionOntology
+from autodistill.detection import DetectionBaseModel
 from PIL import Image
 from transformers import OwlViTForObjectDetection, OwlViTProcessor
 
-from owl_vit_object_detection.src.models import OwlViT
+# from autodistill_owl_vit.owl_vit_object_detection.src.models import OwlViT
+from .models import OwlViT
 
 HOME = os.path.expanduser("~")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -16,39 +17,62 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # model = OwlViTForObjectDetection.from_pretrained("google/owlvit-large-patch14").to(
 #     DEVICE
 # )
-model = OwlViT.load_state_dict(
-    "/home/jovyan/speir-datavol-115/models/owl_models/test.pt"
-).to(DEVICE)
-processor = OwlViTProcessor.from_pretrained("google/owlvit-large-patch14")
+# checkpoint = torch.load("/home/jovyan/speir-datavol-115/models/owl_models/test.pt")
+# model = OwlViT().load_state_dict(checkpoint["model"]).to(DEVICE)
+# model = OwlViT().load_state_dict(torch.load('state_dict_path')).to(DEVICE)
+#     torch.load("/home/jovyan/speir-datavol-115/models/owl_models/test.pt")
+# ).to(DEVICE)
 
 
-@dataclass
-class OWLViT_finetuned(DetectionBaseModel):
-    ontology: CaptionOntology
-    owlvit_model: model
+# @dataclass
+class OWLViT_Finetuned(DetectionBaseModel):
+    # ontology: CaptionOntology
+    # owlvit_model: model
 
-    def __init__(self, ontology: CaptionOntology):
-        self.ontology = ontology
-        self.owlvit_model = model
+    def __init__(self, model_path, labelmap):
+        self.processor = OwlViTProcessor.from_pretrained("google/owlvit-large-patch14")
+        pretrained_model = OwlViTForObjectDetection.from_pretrained(
+            "google/owlvit-large-patch14"
+        )
+
+        to_encode = []
+        for label in labelmap.values():
+            to_encode.append(label)
+            to_encode.append("a photo of " + label)
+            to_encode.append("a " + label + " in an environment")
+
+        inputs = self.processor(
+            text=[to_encode],
+            images=Image.new("RGB", (224, 224)),
+            return_tensors="pt",
+        )
+        with torch.no_grad():
+            queries = pretrained_model(**inputs).text_embeds
+
+        self.ft_model = (
+            OwlViT(pretrained_model=pretrained_model, query_bank=queries)
+            .load_state_dict(torch.load(model_path))
+            .to(DEVICE)
+        )
 
     def predict(self, input: str) -> sv.Detections:
-        labels = self.ontology.prompts()
+        # labels = self.ontology.prompts()
 
         image = Image.open(input).convert("RGB")
 
         with torch.no_grad():
-            if isinstance(self.ontology, CaptionOntology):
-                # inputs = processor(text=labels, images=image, return_tensors="pt").to(
-                #     DEVICE
-                # )
-                image = processor(images=image, return_tensors="pt")[
-                    "pixel_values"
-                ].squeeze(0)
-                # pred_boxes in 'corners' format ?
-                pred_boxes, pred_class_logits, pred_class_sims, _ = model(image)
-            else:
-                print("Not implemented")
-                exit()
+            # if isinstance(self.ontology, CaptionOntology):
+            # inputs = processor(text=labels, images=image, return_tensors="pt").to(
+            #     DEVICE
+            # )
+            image = self.processor(images=image, return_tensors="pt")[
+                "pixel_values"
+            ].squeeze(0)
+            # pred_boxes in 'corners' format ?
+            pred_boxes, pred_class_logits, pred_class_sims, _ = self.ft_model(image)
+            # # else:
+            #     print("Not implemented")
+            #     exit()
 
             # target_sizes = torch.Tensor([image.size[::-1]]).to(DEVICE)
 
