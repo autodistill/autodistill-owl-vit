bd58bb1d9ff3030222d3b2a6dd36215410a93e3a
James Gallagher <jamesg@jamesg.blog>
2023-12-06 09:19:18 +0000

    use load_image

--- autodistill_owl_vit/__init__.py
+++ autodistill_owl_vit/__init__.py
@@ -1,3 +1,3 @@
 from autodistill_owl_vit.owlvit import OWLViT
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
--- autodistill_owl_vit/owlvit.py
+++ autodistill_owl_vit/owlvit.py
@@ -4,15 +4,19 @@ from dataclasses import dataclass
 import numpy as np
 import supervision as sv
 import torch
-from PIL import Image
+from transformers import OwlViTForObjectDetection, OwlViTProcessor
+
 from autodistill.detection import CaptionOntology, DetectionBaseModel
-from transformers import OwlViTProcessor, OwlViTForObjectDetection
+from autodistill.helpers import load_image
 
 HOME = os.path.expanduser("~")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32").to(DEVICE)
+model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32").to(
+    DEVICE
+)
 processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
+
 
 @dataclass
 class OWLViT(DetectionBaseModel):
@@ -23,10 +27,10 @@ class OWLViT(DetectionBaseModel):
         self.ontology = ontology
         self.owlvit_model = model
 
-    def predict(self, input: str) -> sv.Detections:
+    def predict(self, input: str, confidence = 0.1) -> sv.Detections:
         labels = self.ontology.prompts()
 
-        image = Image.open(input)
+        image = load_image(input, return_format="PIL")
 
         with torch.no_grad():
             inputs = processor(text=labels, images=image, return_tensors="pt")
@@ -34,14 +38,28 @@ class OWLViT(DetectionBaseModel):
 
             target_sizes = torch.Tensor([image.size[::-1]])
 
-            results = processor.post_process(outputs=outputs, target_sizes=target_sizes)
+            results = processor.post_process_object_detection(outputs=outputs, target_sizes=target_sizes)
 
             i = 0
 
+            boxes = results[i]["boxes"].tolist()
+            scores = results[i]["scores"].tolist()
+            labels = results[i]["labels"].tolist()
+
+            print(scores)
+
+            # filter with score < confidence
+            boxes = [box for box, score in zip(boxes, scores) if score > confidence]
+            labels = [label for label, score in zip(labels, scores) if score > confidence]
+            scores = [score for score in scores if score > confidence]
+
+            if len(boxes) == 0:
+                return sv.Detections.empty()
+
             detections = sv.Detections(
-                xyxy=np.array(results[i]["boxes"]),
-                class_id=np.array(results[i]["labels"]),
-                confidence=np.array(results[i]["scores"]),
+                xyxy=np.array(boxes),
+                class_id=np.array(labels),
+                confidence=np.array(scores),
             )
 
             return detections
